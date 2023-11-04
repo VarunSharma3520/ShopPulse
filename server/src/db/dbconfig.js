5201b8ca066e234d42ba9cf17d0e5be4f39276b0
VarunSharma3520 <sv279508@gmail.com>
2023-11-04 20:25:48 +0000

    db connection in server

---
+++ server/src/db/dbconfig.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+	try {
+		const connectionInstance = await mongoose.connect(
+			`${process.env.MONGO_DB_URI}}`
+		)
+		await console.log(
+			`connection sucessfull\n DB HOST: ${connectionInstance.connection.host}`
+		)
+	} catch (err) {
+		console.error(`ERROR: ${err}`)
+		process.exit(1)
+	}
+}
+
+export default connectDB
+
+/*
+// writing iife
+;(async () => {
+	try {
+		await mongoose.connect(
+			`${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`
+		)
+		await console.log("connection sucessfull")
+	} catch (err) {
+		console.error(`ERROR${err}`)
+	}
+})()
+*/
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express from "express"
+import connectDB from "./db/dbconfig.js"
+import dotenv from "dotenv"
+dotenv.config({path: "../env"})
+
+connectDB()
+
+const app = express()
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.json({ Hello: "World!" });
-});
+	res.json({Hello: "World"})
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+	console.log(`Example app listening on port ${port}`)
+})
