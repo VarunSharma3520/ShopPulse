ec6ce634a33c0bf6f465e07b7be7f330ef6222e9
VarunSharma3520 <sv279508@gmail.com>
2023-11-06 22:52:58 +0000

    installed cors cookie-parser

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,23 @@
-import express from "express"
+import express, { application, urlencoded } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import connectDB from "./db/dbconfig.js"
 import dotenv from "dotenv"
 import user from "./routes/user.routes.js"
 
-dotenv.config({path: "../env"})
-// connectDB()
-
 const app = express()
 const port = process.env.PORT || 3000
-
+app.use(express.static("public"))
+app.use(cors())
+app.use(express.json({ limit: "16kb" }))
+app.use(urlencoded({ extended: true, limit: "16kb" }))
+app.use(cookieParser())
 app.use("/", user)
+
+
+dotenv.config({path: "../env"})
+connectDB()
+
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
