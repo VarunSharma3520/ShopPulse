de1a6f9ba02d8f67a47698fbaac588c0bad96881
VarunSharma3520 <sv279508@gmail.com>
2023-12-14 04:34:05 +0000

    merged app and server

--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,46 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/dbconfig.js"
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import connectDB from "./db/dbconfig.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// file import
+import connectDB from "./db/dbconfig.js";
+
+//routes import
+import userRouter from './src/routes/user.routes.js';
 const app = express()
 
-app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-		credentials: true,
-	})
-)
-
+app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true}));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
 //routes import
-import userRouter from "./routes/user.routes.js"
+import userRouter from './src/routes/user.routes.js';
+
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 
-app.get("/", (req, res) => {
-	res.send("hello world")
-})
 // http://localhost:8000/api/v1/users/registerdotenv.config({
-// path: "./.env"
+    // path: './.env'
 // })
 
+app.disable('x-powered-by');
+
+
+
 connectDB()
-	.then(() => {
-		app.listen(process.env.PORT || 8000, () => {
-			console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
-		})
-	})
-	.catch((err) => {
-		console.log("MONGO db connection failed !!! ", err)
-	})
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`ctrl + click ${req.protocol}://${req.get("host")}${req.originalUrl}`)
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
