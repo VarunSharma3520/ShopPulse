4068d0d0e76247221066779e55b7df8bf4fb075f
Varun Sharma <110775856+VarunSharma3520@users.noreply.github.com>
2023-12-14 03:18:02 +0000

    Update 5 files

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,22 +1,23 @@
-import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import {Router} from "express"
+import {registerUser} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/user.middlewares.js"
 
 const router = Router()
-
+router.get("/", (req, res) => {
+	res.send("Birds home page")
+})
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    )
-
+	upload.fields([
+		{
+			name: "avatar",
+			maxCount: 1,
+		},
+		{
+			name: "coverImage",
+			maxCount: 1,
+		},
+	]),
+	registerUser
+)
 
 export default router
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,43 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/dbconfig.js";
-import {app} from '../app.js'
-dotenv.config({
-    path: './.env'
+import connectDB from "./db/dbconfig.js"
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	})
+)
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+app.get("/", (req, res) => {
+	res.send("hello world")
 })
-
-
+// http://localhost:8000/api/v1/users/registerdotenv.config({
+// path: "./.env"
+// })
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+	.then(() => {
+		app.listen(process.env.PORT || 8000, () => {
+			console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
+		})
+	})
+	.catch((err) => {
+		console.log("MONGO db connection failed !!! ", err)
+	})
