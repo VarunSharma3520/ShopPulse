8d4afd0660904eeac24892897369dca150634f41
VarunSharma3520 <sv279508@gmail.com>
2023-12-14 05:15:43 +0000

    login routes

--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,35 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import express from "express";
-import connectDB from "./db/dbconfig.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
+
 
 // file import
 import connectDB from "./db/dbconfig.js";
 
 //routes import
-import userRouter from './src/routes/user.routes.js';
+import userRouter from './routes/user.routes.js';
+import loginRouter from './routes/login.routes.js';
+
+// app intialization 
 const app = express()
 
-app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true}));
+//routes declaration
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/login", loginRouter)
+
+app.use(cors());
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
-
-//routes import
-import userRouter from './src/routes/user.routes.js';
-
-
-//routes declaration
-app.use("/api/v1/users", userRouter)
-
-// http://localhost:8000/api/v1/users/registerdotenv.config({
-    // path: './.env'
-// })
-
 app.disable('x-powered-by');
-
-
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`ctrl + click ${req.protocol}://${req.get("host")}${req.originalUrl}`)
+    app.listen(process.env.PORT || 5000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
